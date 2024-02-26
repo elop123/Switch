a01e42aa823e7fc299f31380e2b4ae57407a614d
elop123 <142408289+elop123@users.noreply.github.com>
2024-02-26 12:07:23 +0100

    new

---
+++ index.js
@@ -0,0 +1,21 @@
+//swtich structure
+
+let myValue=100
+
+//standard switch and default
+switch (myValue) {
+    case 50:
+        console.log('Var er 50');
+        break;
+
+    case 75:
+        console.log('Var er 75');
+        break;
+    case 100:
+        console.log('Var er 100');
+        break;
+
+    default:
+        console.log('Default');
+        break;
+}
